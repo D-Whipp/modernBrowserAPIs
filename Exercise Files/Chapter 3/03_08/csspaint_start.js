32185906766d8b082c326c049b7c5f093e6f28f7
David Whipple <dwhipp88@gmail.com>
2022-07-22 18:25:51 -0600

    explored houdini api with dynamic paint

--- Exercise Files/Chapter 3/03_08/csspaint_start.js
+++ Exercise Files/Chapter 3/03_08/csspaint_start.js
@@ -1,26 +1,31 @@
 if (typeof registerPaint !== 'undefined') {
-    // define a class to implement the paint worklet
-    class SampleCSSPaint {
-        // TODO: declare the properties that the class has access to
-
-        
-        // TODO: fill out the paint function to do the drawing work
-        paint(ctx, size, props) {
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = "blue";
-
-            ctx.beginPath();
-            ctx.moveTo(0,0);
-            ctx.lineTo(size.width,size.height);
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.moveTo(size.width,0);
-            ctx.lineTo(0,size.height);
-            ctx.stroke();
-        }
+  // define a class to implement the paint worklet
+  class SampleCSSPaint {
+    // TODO: declare the properties that the class has access to
+    static get inputProperties() {
+      return ['--cross-thickness', '--cross-color'];
     }
 
-    // TODO: register the paint worklet for CSS
-    registerPaint("samplepainter", SampleCSSPaint);
+    // TODO: fill out the paint function to do the drawing work
+    paint(ctx, size, props) {
+      let width = props.get('--cross-thickness');
+      let color = props.get('--cross-color').toString();
+
+      ctx.lineWidth = width;
+      ctx.strokeStyle = color;
+
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(size.width, size.height);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(size.width, 0);
+      ctx.lineTo(0, size.height);
+      ctx.stroke();
+    }
+  }
+
+  // TODO: register the paint worklet for CSS
+  registerPaint('samplepainter', SampleCSSPaint);
 }
